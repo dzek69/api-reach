dbc8ab0d26cd01e9e4b56edf9a72aa5aacc060c8
dzek <git-public@dzek.eu>
2024-05-17 21:03:58 +0200

    ! api endpoints object won't trigger missing index type error anymore

--- src/types/common.ts
+++ src/types/common.ts
@@ -17,6 +17,18 @@ type GenericBody = RecordLike<unknown> | string | undefined;
 type GenericQuery = RecordLike<unknown> | undefined;
 type GenericHeaders = RecordLike<string> | undefined;
 
+type RemoveIndex<T> = {
+    [ K in keyof T as
+    string extends K
+        ? never
+        : number extends K
+            ? never
+            : symbol extends K
+                ? never
+                : K
+    ]: T[K];
+};
+
 /**
  * This type is a constraint for the ApiClient class, it represents a generic list of responses for methods and urls
  */
@@ -24,24 +36,24 @@ type ApiEndpoints = RecordLike<RecordLike<{
     /**
      * Expected response type (not validated, just assumed)
      */
-    response: GenericJSONResponse;
+    response: RemoveIndex<GenericJSONResponse>;
     /**
      * Params list (to replace `:param` in url)
      */
-    params?: GenericParams;
+    params?: RemoveIndex<GenericParams>;
     /**
      * Request body data type
      */
-    body?: GenericBody;
+    body?: RemoveIndex<GenericBody>;
     bodyType?: RequestBodyType;
     /**
      * URL Query data type
      */
-    query?: GenericQuery;
+    query?: RemoveIndex<GenericQuery>;
     /**
      * Headers data type
      */
-    headers?: GenericHeaders;
+    headers?: RemoveIndex<GenericHeaders>;
 }>>;
 
 type ValidateApiEndpoints<T extends ApiEndpoints> = T;
